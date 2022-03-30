a3179440462b557b5c6a9ee20194f58481e802f6
karolsrod <karolinesrod@gmail.com>
2022-03-30 15:03:47 -0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Main {
     public static void main (String[] args){
@@ -14,16 +14,51 @@ public class Main {
         Curso curso2 = new Curso();
         curso2.setTitulo("curso js");
         curso2.setDescricao("descricao curso js");
-        curso2.setCargaHoraria(8);
+        curso2.setCargaHoraria(4);
 
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("mentoria de java");
         mentoria.setDescricao("descricao mentoria de java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descricao Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devKarol = new Dev();
+        devKarol.setNome("Karol");
+        devKarol.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos Karol" + devKarol.getConteudoInscrito());
+        devKarol.progredir();
+        devKarol.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos Karol" + devKarol.getConteudoInscrito());
+        System.out.println("Conteudos Concluidos Karol" + devKarol.getConteudoConcluido());
+        System.out.println("XP:" + devKarol.calcularTotalXp());
+
+        System.out.println("----------");
+
+        Dev devVitor = new Dev();
+        devVitor.setNome("Vitor");
+        devVitor.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos Vitor" + devVitor.getConteudoInscrito());
+        devVitor.progredir();
+        devVitor.progredir();
+        devVitor.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos Vitor" + devVitor.getConteudoInscrito());
+        System.out.println("Conteudos Concluidos Vitor" + devVitor.getConteudoConcluido());
+        System.out.println("XP:" + devVitor.calcularTotalXp());
+
+
+
 
     }
 }
--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -1,43 +1,36 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso {
-    private String titulo;
-    private String descricao;
+public class Curso extends Conteudo{
+
     private int cargaHoraria;
 
     public Curso() {
     }
 
-    public String getTitulo() {
-        return titulo;
-    }
+        public int getCargaHoraria(){
+            return cargaHoraria;
+        }
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
+        public void setCargaHoraria ( int cargaHoraria){
+            this.cargaHoraria = cargaHoraria;
+        }
 
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public int getCargaHoraria() {
-        return cargaHoraria;
-    }
-
-    public void setCargaHoraria(int cargaHoraria) {
-        this.cargaHoraria = cargaHoraria;
-    }
+        @Override
+        public String toString () {
+            return "Curso{" +
+                    "titulo='" + getTitulo() + '\'' +
+                    ", descricao='" + getDescricao() + '\'' +
+                    ", cargaHoraria=" + cargaHoraria +
+                    '}';
+        }
 
     @Override
-    public String toString() {
-        return "Curso{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
-                ", cargaHoraria=" + cargaHoraria +
-                '}';
+    public double calculaXp() {
+        return 0;
     }
 }
+
+
+
+
+
